5e2e1f2666b5317b21b994330d9914745035ec41
SpRajib <sprajiblochansahoo@gmail.com>
2025-08-23 09:19:44 +0530

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,202 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+// import webverseLogo from './assets/company_logo/webverse_logo.png';
+// import agcLogo from './assets/company_logo/agc_logo.png';
+// import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import soaLogo from './assets/education_logo/SOA_PNG.png';
+import chseLogo from './assets/education_logo/CHSE_logo.png';
+import bseLogo from './assets/education_logo/BSE_logo.avif';
+
+// Project Section Logo's
+import csprepLogo from './assets/work_logo/Bld_grp.png';
+import movierecLogo from './assets/work_logo/dbts.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      // { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      // { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      // { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      // { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      // { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      // { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      // { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: webverseLogo,
+      role: "Fullstack Developer",
+      company: "Webverse Digital",
+      date: "April 2024 - Present",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: soaLogo,
+      school: "ITER,SOA University, Bhubaneswar",
+      date: "Sept 2023 - July 2025",
+      grade: "7.81 CGPA",
+      desc: "I have completed my Master's degree (MCA) in Computer Applications from Siksha 'O' Anusandhan University, Bhubaneswar. During my time at ITER, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at ITER, SOA University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Master of Computer Applications - MCA",
+    },
+    {
+      id: 1,
+      img: soaLogo,
+      school: "ITER , SOA University, Bhubaneswar",
+      date: "Sept 2020 - July 2023",
+      grade: "8.02 CGPA",
+      desc: "I completed my Bachelor's degree in Computer Application (BCA) from Institute of Technical Education & Research, Bhubaneswar. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+      degree: "Bachelor of Computer Application - BCA",
+    },
+    {
+      id: 2,
+      img: chseLogo,
+      school: "Saraswati Gyan Mandir, Bhubaneswar",
+      date: "Apr 2018 - March 2020",
+      grade: "54.33%",
+      desc: "I completed my class 12 education from Saraswati Gyan Mandir, Bhubaneswar under the CHSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Biology.",
+      degree: "CBSE(XII) - PCM with Biology",
+    },
+    {
+      id: 3,
+      img: bseLogo,
+      school: "Saraswati Vidya Mandir, Baramunda",
+      date: "Apr 2017 - March 2018",
+      grade: "72%",
+      desc: "I completed my class 10 education from Saraswati Vidya Mandir, Baramunda, under the Odisha board (BSE), where I studied Science.",
+      degree: "BSE(X), Science",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Blood Group Prediction using ML",
+      description:
+        "This project focuses on developing a machine learning model that predicts a person’s blood group based on biometric or medical attributes. The system takes input features such as fingerprint patterns, genetic traits, or health-related parameters and applies supervised learning algorithms to classify individuals into their respective blood groups (A, B, AB, O). The trained model is integrated into a web application for easy access, allowing users to upload input data and receive instant predictions. This project demonstrates the application of machine learning in healthcare, improving accuracy, efficiency, and automation in blood group identification.",
+      image: csprepLogo,
+      tags: ["Python","Tensorflow","AI & ML", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/SpRajib/BLOOD-GROUP-DETECTION-USING-ML.git",
+      webapp: "#",
+    },
+    {
+      id: 1,
+      title: "Diabetes Prediction using ML",
+      description:
+        "This project aims to build a machine learning model that predicts whether a person is diabetic based on medical attributes such as glucose level, BMI, blood pressure, insulin, and age. The system uses supervised learning algorithms to classify patients into diabetic or non-diabetic categories. The model is deployed through a Flask-based web application, allowing users to enter their health parameters and receive instant predictions. This project highlights how machine learning can assist in early diagnosis and decision-making in healthcare.",
+      image: movierecLogo,
+      tags: ["Python","numpy", "scikit-learn", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/SpRajib/Diabetes_Prediction.git",
+      webapp: "#",
+    },
+    {
+      id: 2,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "#",
+      webapp: "#",
+    },
+  ];  
